b6079a1304a4f8f6f0bcc5e511fe5cfb3775a1b9
Massimo Gervasini <mgervasini@mozilla.com>
2012-10-23 16:57:59 +0200

    ported init.pp form slave

--- modules/buildmaster/manifests/init.pp
+++ modules/buildmaster/manifests/init.pp
@@ -1,23 +1,16 @@
-# This module is responsible for installing and running buildbot-slave on all
-# slaves.  Do not be confused by the name 'buildslave' - this applies to all
-# slaves used in releng -- build, test, whatever.
-#
-# See:
-#  - https://wiki.mozilla.org/ReleaseEngineering/Buildslave_Startup_Process
-#  - https://wiki.mozilla.org/ReleaseEngineering/Buildslave_Versions
-class buildslave {
+class buildmaster {
     anchor {
-        'buildslave::begin': ;
-        'buildslave::end': ;
+        'buildmaster::begin': ;
+        'buildmaster::end': ;
     }
 
-    Anchor['buildslave::begin'] ->
+    Anchor['buildmaster::begin'] ->
     class {
-        'buildslave::install': ;
-    } -> Anchor['buildslave::end']
+        'buildmaster::install': ;
+    } -> Anchor['buildmaster::end']
 
-    Anchor['buildslave::begin'] ->
+    Anchor['buildmaster::begin'] ->
     class {
-        'buildslave::startup': ;
-    } -> Anchor['buildslave::end']
+        'buildmaster::startup': ;
+    } -> Anchor['buildmaster::end']
 }

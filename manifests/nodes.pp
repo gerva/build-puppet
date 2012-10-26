2f5526cd7355ecaa35738a6ddd35921411043a6e
Massimo Gervasini <mgervasini@mozilla.com>
2012-10-26 11:24:04 +0200

    relabs07 is now a buildmaster node

--- manifests/nodes.pp
+++ manifests/nodes.pp
@@ -3,7 +3,7 @@ node /bld-centos6-hp-\d+.build.scl1.mozilla.com/ {
 }
 
 node "relabs07.build.mtv1.mozilla.com" {
-    include toplevel::slave::build
+    include toplevel::slave::buildmaster
 }
 
 node "relabs08.build.mtv1.mozilla.com" {

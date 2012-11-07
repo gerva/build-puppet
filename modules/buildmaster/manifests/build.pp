8fcaefd9621c7d2ca8197c506022e7ab4ca1f559
Massimo Gervasini <mgervasini@mozilla.com>
2012-11-07 12:15:12 +0100

    added build.pp

---
+++ modules/buildmaster/manifests/build.pp
@@ -0,0 +1,7 @@
+class buildmaster::build {
+    http_port => 8001,
+    master_type => "build",
+    basedir => "build1",
+    buildslaves_template => 'BuildSlaves-build.py.erb',
+    include buildmaster::install
+}

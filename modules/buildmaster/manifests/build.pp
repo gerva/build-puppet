9948ba8334fda321dbbea743ca42b4a88ab101b1
Massimo Gervasini <mgervasini@mozilla.com>
2012-11-08 11:14:09 +0100

    fixed typo in build.pp

--- modules/buildmaster/manifests/build.pp
+++ modules/buildmaster/manifests/build.pp
@@ -2,6 +2,6 @@ class buildmaster::build {
     $http_port = 8001
     $master_type = "build"
     $basedir = "build1"
-    $buildslaves_template = 'BuildSlaves-build.py.erb',
+    $buildslaves_template = 'BuildSlaves-build.py.erb'
     include buildmaster::install
 }

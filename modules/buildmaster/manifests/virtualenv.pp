1e6a226344b7c50b0fb1f3c2212c28e8dbde95f4
Massimo Gervasini <mgervasini@mozilla.com>
2012-11-14 17:27:20 +0100

    using python27 for virtualenvs

--- modules/buildmaster/manifests/virtualenv.pp
+++ modules/buildmaster/manifests/virtualenv.pp
@@ -1,10 +1,11 @@
 class buildmaster::virtualenv {
     include buildmaster::settings
-    include packages::mozilla
+    include packages::mozilla::python27
 
     python::virtualenv {
         "$buildmaster::settings::master_queue_venv"
-            python => "/usr/bin/python2.6",
+            python => "/tools/python27/bin/python2.7",
+            require => Class['packages::mozilla::python27'],
             packages => [
                 "simplejson",
                 "buildbot==0.8.4-pre-moz1",

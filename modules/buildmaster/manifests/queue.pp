66bf3286cabde52b584e1f59a21e69c8474e1122
Massimo Gervasini <mgervasini@mozilla.com>
2012-11-15 12:41:23 +0100

    removed dependencies from settings

--- modules/buildmaster/manifests/queue.pp
+++ modules/buildmaster/manifests/queue.pp
@@ -3,28 +3,28 @@
 
 class buildmaster::queue {
     include config
+    include users::builder
     include buildmaster::settings
     include buildmaster::virtualenv
-    $master_basedir = $buildmaster::settings::master_basedir
-    $master_user = $buildmaster::settings::master_user
-    $master_group = $buildmaster::settings::master_group
-    $master_queue_venv = $buildmaster::settings::master_queue_venv
+
+    $queue_venv_dir = "$users::builder::home/queue"
+
     exec {
         # Clone/install tools
         "clone-tools":
             require => [
                         Package["mercurial"],
-                        Python::Virtualenv[$master_queue_venv],
+                        Python::Virtualenv[$queue_venv_dir],
                        ],
-            creates => "$master_queue_venv/tools",
-            command => "/usr/bin/hg clone http://hg.mozilla.org/build/tools $master_queue_venv/tools",
-            user => config::builder_username;
+            creates => "$queue_venv_dir/tools",
+            command => "/usr/bin/hg clone http://hg.mozilla.org/build/tools $queue_venv_dir/tools",
+            user => $users::builder::username;
         "install-tools":
             require => Exec["clone-tools"],
-            creates => "$master_queue_venv/lib/python2.6/site-packages/buildtools.egg-link",
-            command => "$master_queue_venv/bin/python setup.py develop",
-            cwd => "$master_queue_venv/tools",
-            user => config::builder_username;
+            creates => "$queue_venv_dir/lib/python2.7/site-packages/buildtools.egg-link",
+            command => "$queue_venv_dir/bin/python setup.py develop",
+            cwd => "$queue_venv_dir/tools",
+            user => $users::builder::username;
     }
 
     file {
@@ -34,8 +34,8 @@ class buildmaster::queue {
             mode => 755,
             owner => "root",
             group => "root";
-        "$buildmaster::settings::master_queue_venv/run_command_runner.sh":
-            require => Python::Virtualenv["$buildmaster::settings::master_queue_venv"],
+        "$queue_venv_dir/run_command_runner.sh":
+            require => Python::Virtualenv["$queue_venv_dir"],
             content => template("buildmaster/run_command_runner.sh.erb"),
             notify => Service["command_runner"],
             mode => 755,
@@ -48,26 +48,25 @@ class buildmaster::queue {
             mode => 644,
             owner => "root",
             group => "root";
-
         "/etc/init.d/pulse_publisher":
             content => template("buildmaster/pulse_publisher.initd.erb"),
             notify => Service["pulse_publisher"],
             mode => 755,
             owner => "root",
             group => "root";
-        "$buildmaster::settings::master_queue_venv/run_pulse_publisher.sh":
-            require => Python::Virtualenv["$buildmaster::settings::master_queue_venv"],
+        "$queue_venv_dir/run_pulse_publisher.sh":
+            require => Python::Virtualenv["$queue_venv_dir"],
             content => template("buildmaster/run_pulse_publisher.sh.erb"),
             notify => Service["pulse_publisher"],
             mode => 755,
             owner => "root",
             group => "root";
-        "$buildmaster::settings::master_queue_venv/passwords.py":
-            require => Python::Virtualenv["$buildmaster::settings::master_queue_venv"],
+        "$queue_venv_dir/passwords.py":
+            require => Python::Virtualenv["$queue_venv_dir"],
             content => template("buildmaster/passwords.py.erb"),
             mode => 600,
-            owner => $master_user,
-            group => $master_group;
+            owner => $users::builder::username,
+            group => $users::builder::group;
         "/etc/init.d/nrpe.d/pulse_publisher.cfg":
             content => template("buildmaster/pulse_publisher.cfg.erb"),
             notify => Class["nrpe::service"],
@@ -80,9 +79,9 @@ class buildmaster::queue {
         "command_runner":
             hasstatus => true,
             require => [
-                Python::Virtualenv["$buildmaster::settings::master_queue_venv"],
+                Python::Virtualenv["$queue_venv_dir"],
                 File["/etc/init.d/command_runner"],
-                File["$buildmaster::settings::master_queue_venv/run_command_runner.sh"],
+                File["$queue_venv_dir/run_command_runner.sh"],
                 Exec["install-tools"],
                 ],
             enable => true,
@@ -90,7 +89,7 @@ class buildmaster::queue {
         "pulse_publisher":
             hasstatus => true,
             require => [
-                Python::Virtualenv["$buildmaster::settings::master_queue_venv"],
+                Python::Virtualenv["$queue_venv_dir"],
                 File["/etc/init.d/pulse_publisher"],
                 File["${master_queue_venv}/run_pulse_publisher.sh"],
                 Exec["install-tools"],

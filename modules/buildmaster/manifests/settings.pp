2d43cce702a2ddbaea1601be3a3d3eb392f19242
Massimo Gervasini <mgervasini@mozilla.com>
2012-11-19 11:09:11 +0100

    fixed typo

--- modules/buildmaster/manifests/settings.pp
+++ modules/buildmaster/manifests/settings.pp
@@ -1,7 +1,7 @@
 class buildmaster::settings {
     include users::builder
 
-    $buildmaster_user = $users::builder::username;
+    $buildmaster_user = $users::builder::username
     $buildmaster_group = $users::builder::group
     $buildmaster_home = $users::builder::home
     $queue_venv_dir = "$build_home/queue"

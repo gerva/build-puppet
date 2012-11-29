84e21705beba274eafe628587fdf9a47c19a983b
Massimo Gervasini <mgervasini@mozilla.com>
2012-11-29 15:29:56 +0100

    work in progress on setup-buildbot

--- modules/buildmaster/manifests/repos.pp
+++ modules/buildmaster/manifests/repos.pp
@@ -9,72 +9,70 @@ class buildmaster::repos {
     include packages::mozilla::py27_mercurial
     include buildmaster::settings
 
+    $master_name='build'
+    $full_master_dir="${buildmaster::settings::master_basedir}/$master_name"
+
     file {
-        "${buildmaster::settings::queue_venv_dir}":
+        "${full_master_dir}":
             owner => $users::builder::username,
             group => $users::builder::group,
             ensure => directory,
             mode => 0755;
     }
 
+    $hg_repo = "http://hg.mozilla.org/build/"
     exec {
         # Clone buildbot
         "clone-buildbot":
             require => [
                 Class['packages::mozilla::py27_mercurial'],
-                File["${buildmaster::settings::queue_venv_dir}"],
+                File["${full_master_dir}"],
             ],
-            creates => "${buildmaster::settings::master_basedir}/buildbot",
-            command => "/tools/python27-mercurial/bin/hg clone http://hg.mozilla.org/build/buildbot ${buildmaster::settings::master_basedir}/buildbot",
+            creates => "${full_master_dir}/buildbot",
+            command => "/tools/python27-mercurial/bin/hg clone ${hg_repo}/buildbot ${full_master_dir}/buildbot",
             user => "$users::builder::username";
         # Clone/install tools and buildbot-configs
         "clone-tools":
             require => [
                 Class['packages::mozilla::py27_mercurial'],
-                File["${buildmaster::settings::queue_venv_dir}"],
+                File["${full_master_dir}"],
             ],
-            creates => "${buildmaster::settings::queue_venv_dir}/tools",
-            command => "/tools/python27-mercurial/bin/hg clone http://hg.mozilla.org/build/tools ${buildmaster::settings::queue_venv_dir}/tools",
+            creates => "${full_master_dir}/tools",
+            command => "/tools/python27-mercurial/bin/hg clone ${hg_repo}/tools ${full_master_dir}/tools",
             user => "$users::builder::username";
         "clone-configs":
             require => [
                 Class['packages::mozilla::py27_mercurial'],
-                File["${buildmaster::settings::queue_venv_dir}"],
+                File["${full_master_dir}"],
             ],
-            creates => "${buildmaster::settings::queue_venv_dir}/buildbot-configs",
-            command => "/tools/python27-mercurial/bin/hg clone http://hg.mozilla.org/build/buildbot-configs ${buildmaster::settings::queue_venv_dir}/buildbot-configs",
+            creates => "${full_master_dir}/buildbot-configs",
+            command => "/tools/python27-mercurial/bin/hg clone ${hg_repo}/buildbot-configs ${full_master_dir}/buildbot-configs",
             user => "$users::builder::username";
     }
 
-    $master_name='build1'
-    $full_master_dir="${buildmaster::settings::master_basedir}/$master_name"
-    exec {
-        # install tools
-        "install-tools":
-            require => Exec["clone-tools"],
-            creates => "${buildmaster::settings::queue_venv_dir}/lib/python2.7/site-packages/buildtools.egg-link",
-            command => "/tools/python27/bin/python2.7 setup.py develop",
-            cwd => "${buildmaster::settings::queue_venv_dir}/tools",
-            user => $users::buildmaster::username;
-    }
+    #exec {
+    #    # install tools
+    #    "install-tools":
+    #        require => Exec["clone-tools"],
+    #        creates => "${buildmaster::settings::queue_venv_dir}/lib/python2.7/site-packages/buildtools.egg-link",
+    #        command => "/tools/python27/bin/python2.7 setup.py develop",
+    #        cwd => "${buildmaster::settings::queue_venv_dir}/tools",
+    #        user => $users::buildmaster::username;
+    #}
 
     exec {
         "setup-buildbot":
             require => [
                 Exec["clone-buildbot"],
         ],
-            command => "make -f Makefile.setup all BASEDIR=${buildmaster::settings::master_basedir} MASTER_NAME=build'",
-            creates => "$full_master_dir/master",
+            command => "make -f Makefile.setup all BASEDIR=${full_master_dir} MASTER_NAME=$master_name'",
+            creates => "${full_master_dir}/master",
             user => $users::buildmaster::username,
             environment => [
-                "HG=/tools/python27-mercurial/bin/hg",
-                "VIRTUALENV=/usr/bin/virtualenv-2.7",
-                "PYTHON=/usr/local/bin/virtualenv",
-                "PIP_DOWNLOAD_CACHE=${buildmaster::settings::master_basedir}/pip_cache",
-                "PIP_FLAGS=--no-deps --no-index #--find-links=$python_package_dir",
-                "MASTERS_JSON=http://hg.mozilla.org/build/tools/raw-file/default/buildfarm/maintenance/production-masters.json-wrongonpurpose",
+                "PYTHON=${buildmaster::s}/build/bin/virtualenv",
+                #"MASTERS_JSON=http://hg.mozilla.org/build/tools/raw-file/default/buildfarm/maintenance/production-masters.json-wrongonpurpose",
             ],
             # --find_links is disabled for now
-            cwd => "$master_basedir/buildbot-configs";
+            cwd => "${buildmaster::settings::master_basedir}/buildbot-configs";
     }
 }

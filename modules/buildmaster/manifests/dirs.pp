9f84d320703cc9e3de4a9d00f11ef961056c38bb
Massimo Gervasini <mgervasini@mozilla.com>
2012-12-04 11:24:40 +0100

    fixes for dirs.pp

--- modules/buildmaster/manifests/dirs.pp
+++ modules/buildmaster/manifests/dirs.pp
@@ -1,14 +1,14 @@
 class buildmaster::dirs {
-    import buildmaster::settings
+    include buildmaster::settings
     file {
         [ "${buildmaster::settings::master_root}",
           "${buildmaster::settings::queue_dir}",
           "${buildmaster::settings::master_basedir}",
           "${buildmaster::settings::master_dir}",
-        ]
-            owner => $users::builder::username,
-            group => $users::builder::group,
-            ensure => directory,
-            mode => 0755;
+        ],
+        owner => $users::builder::username,
+        group => $users::builder::group,
+        ensure => directory,
+        mode => 0755;
     }
 }

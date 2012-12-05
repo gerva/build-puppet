5c425f963596ce2035d5d3b478cac262b73348cf
Massimo Gervasini <mgervasini@mozilla.com>
2012-12-05 18:06:37 +0100

    added buildbot_master.pp from puppet-manifest

--- modules/buildmaster/manifests/init.pp
+++ modules/buildmaster/manifests/init.pp
@@ -1,1 +1,66 @@
-
+# buildmaster class
+# include this in your node to have all the base requirements for running a
+# buildbot master installed
+# To setup a particular instance of a buildbot master, see
+# buildmaster::buildbot_master
+#
+# buildmaster requires that $num_masters be set on the node prior to including this class
+#
+# TODO: move $libdir stuff into template?
+# TODO: you still have to set up ssh keys!
+# TODO: determine num_masters from json (bug 647374)
+class buildmaster {
+    include releng::master
+    include secrets
+    include buildmaster::queue
+    include buildmaster::settings
+    $master_basedir = $buildmaster::settings::master_basedir
+    if $num_masters == '' {
+        fail("you must set num_masters")
+    }
+    service {
+        "buildbot":
+            require => File["/etc/init.d/buildbot"],
+            enable => true;
+    A
+    }
+    $plugins_dir = $nagios::service::plugins_dir
+    $nagios_etcdir = $nagios::service::etcdir
+    file {
+        "/builds":
+            ensure => directory,
+            owner => $users::builder::username,
+            group => $users::builder::group;
+        $master_basedir:
+            ensure => directory,
+            owner => $users::builder::username,
+            group => $users::builder::group;
+        "/etc/default/buildbot.d/":
+            mode => 755,
+            ensure => directory;
+        "/etc/init.d/buildbot":
+            source => "puppet:///modules/buildmaster/buildbot.initd",
+            mode => 755,
+        "/root/.my.cnf":
+            content => template("buildmaster/my.cnf.erb"),
+            mode => 600,
+            owner => "root",
+            group => "root";
+        "${nagios_etcdir}/nrpe.d/buildbot.cfg":
+            content => template("buildmaster/buildbot.cfg.erb"),
+            notify => Service["nrpe"],
+            require => Class["nagios"],
+            mode => 644,
+            owner => "root",
+            group => "root";
+        "/tools":
+            ensure => "directory";
+    }
+    exec {
+        "clone-configs":
+            creates => "$master_basedir/buildbot-configs",
+            command => "/usr/bin/hg clone -r production http://hg.mozilla.org/build/buildbot-configs",
+            user => $users::builder::username,
+            cwd => $master_basedir;
+    }
+}
--- modules/buildmaster/manifests/settings.pp
+++ modules/buildmaster/manifests/settings.pp
@@ -1,12 +1,5 @@
 class buildmaster::settings {
-    include users::builder
 
     $master_root = "/builds/buildbot"
     $queue_dir = "${master_root}/queue"
-    $master_basedir = "${master_root}/${master_name}"
-    $master_dir = "${master_basedir}/${master_type}"
-    $tools_dir = "${master_dir}/tools"
-    $buildbot_dir = "${master_dir}/buildbot"
-    $virtualenv_dir = "${master_dir}/venv"
-    $buildbot_configs_dir ="${master_dir}/buildbot-configs"
 }

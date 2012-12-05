909270734c27bdc6ce54debbbb9c0a620e2d8a66
Massimo Gervasini <mgervasini@mozilla.com>
2012-12-05 13:19:44 +0100

    removed cloning of tools and buildbot; make will do it automatically

--- modules/buildmaster/manifests/repos.pp
+++ modules/buildmaster/manifests/repos.pp
@@ -4,22 +4,8 @@ class buildmaster::repos {
 
     $hg_repo = "http://hg.mozilla.org/build/"
     exec {
-        # Clone buildbot
-        "clone-buildbot":
-            require => [
-                Class['packages::mozilla::py27_mercurial'],
-            ],
-            creates => "${buildmaster::settings::buildbot_dir}",
-            command => "/tools/python27-mercurial/bin/hg clone ${hg_repo}/buildbot ${buildmaster::settings::buildbot_dir}",
-            user => "$users::builder::username";
-        # Clone/install tools and buildbot-configs
-        "clone-tools":
-            require => [
-                Class['packages::mozilla::py27_mercurial'],
-            ],
-            creates => "${buildmaster::settings::tools_dir}",
-            command => "/tools/python27-mercurial/bin/hg clone ${hg_repo}/tools ${buildmaster::settings::tools_dir}",
-            user => "$users::builder::username";
+        # make will take care of checking out
+        # buildbotcustom and tools
         "clone-configs":
             require => [
                 Class['packages::mozilla::py27_mercurial'],
@@ -28,14 +14,4 @@ class buildmaster::repos {
             command => "/tools/python27-mercurial/bin/hg clone ${hg_repo}/buildbot-configs ${buildmaster::settings::buildbot_configs_dir}",
             user => "$users::builder::username";
     }
-
-    #exec {
-    #    # install tools
-    #    "install-tools":
-    #        require => Exec["clone-tools"],
-    #        creates => "${buildmaster::settings::queue_venv_dir}/lib/python2.7/site-packages/buildtools.egg-link",
-    #        command => "/tools/python27/bin/python2.7 setup.py develop",
-    #        cwd => "${buildmaster::settings::queue_venv_dir}/tools",
-    #        user => $users::buildmaster::username;
-    #}
 }

5fdfe053a4e2b3cfe94bc8a89548f5879ce1c48b
Massimo Gervasini <mgervasini@mozilla.com>
2012-12-05 15:22:43 +0100

    replaced make stuff with make --help

--- modules/buildmaster/manifests/install.pp
+++ modules/buildmaster/manifests/install.pp
@@ -25,18 +25,10 @@ class buildmaster::install {
             enable => true;
     }
 
-    $test_ven="VIRTUALENV=${buildmaster::settings::virtualenv_dir}"
-    $test_py="PTYHON=${buildmaster::settings::virtualenv_dir}/bin/python"
-    $test_cwd="CWD=>$buildmaster::settings::buildbot_configs_dir"
     exec {
         "make-buildbot":
             require => Class['buildmaster::virtualenv'],
-            command => "echo '$test_ven $test_py $test_cwd';/usr/bin/make -f Makefile.setup all BASEDIR=${buildmaster::settings::master_dir} MASTER_NAME=${buildmaster::settings::master_name}",
+            command => "/usr/bin/make --help","
             user => $users::buildmaster::username,
-            cwd => $buildmaster::settings::buildbot_configs_dir,
-            environment => [
-             "VIRTUALENV=${buildmaster::settings::virutalenv_dir}",
-             "PTYHON=${buildmaster::settings::virtualenv_dir}/bin/python"
-            ],
     }
 }

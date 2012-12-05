31c286d23e6faff308423243b7dd2ed5058b3993
Massimo Gervasini <mgervasini@mozilla.com>
2012-12-05 12:24:26 +0100

    moved common modules into toplevel/buildmaster

--- modules/buildmaster/manifests/install.pp
+++ modules/buildmaster/manifests/install.pp
@@ -10,22 +10,10 @@
 # TODO: you still have to set up ssh keys!
 # TODO: determine num_masters from json (bug 647374)
 class buildmaster::install {
-    include nrpe::base
-    include users::builder
-    include dirs::builds::buildmaster
-    include packages::gcc
-    include packages::make
-    include packages::mercurial
-    include packages::mysql-devel
-    include packages::mozilla::git
-    include packages::mozilla::python27
-    include packages::mozilla::py27_virtualenv
-    include packages::mozilla::py27_mercurial
     include buildmaster::settings
     include buildmaster::virtualenv
     include buildmaster::repos
     include buildmaster::queue
-
 
     if $num_masters == '' {
         fail("you must set num_masters")
--- modules/toplevel/manifests/server/buildmaster.pp
+++ modules/toplevel/manifests/server/buildmaster.pp
@@ -4,6 +4,17 @@ class toplevel::server::buildmaster inherits toplevel::server {
 
     #include packages::mercurial
     #include ::buildmaster
+    include nrpe::base
+    include users::builder
+    include dirs::builds::buildmaster
+    include packages::gcc
+    include packages::make
+    include packages::mercurial
+    include packages::mysql-devel
+    include packages::mozilla::git
+    include packages::mozilla::python27
+    include packages::mozilla::py27_virtualenv
+    include packages::mozilla::py27_mercurial
     include buildmaster
 }
 

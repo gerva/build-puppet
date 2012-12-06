d9d5d1a1ca0dbf2b2cb5e01474bcd9704e06a8d8
Massimo Gervasini <mgervasini@mozilla.com>
2012-12-06 16:01:52 +0100

    added missing ;

--- modules/dirs/manifests/builds/buildmaster.pp
+++ modules/dirs/manifests/builds/buildmaster.pp
@@ -6,19 +6,19 @@ class dirs::builds::buildmaster {
 
     file {
         "/etc/default/buildbot.d/":
-            mode => 755,
-            ensure => directory;
+            ensure => directory,
+            mode => 755;
         "/etc/init.d/buildbot":
             source => "puppet:///modules/buildmaster/buildbot.initd",
-            mode => 755,
+            mode => 755;
         "/root/.my.cnf":
             content => template("buildmaster/my.cnf.erb"),
-            mode => 600,
+            mode => 600;
         "/etc/nagios/nrpe.d/buildbot.cfg":
             content => template("buildmaster/buildbot.cfg.erb"),
             notify => Class["nrpe::service"],
             require => Package["nrpe"],
-            mode => 644,
+            mode => 644;
     }
 }
 

098459bb4e47b4f06c985fca802a2469915dee8e
Massimo Gervasini <mgervasini@mozilla.com>
2013-01-30 17:14:39 +0100

    added required python packages for tools

--- modules/buildmaster/manifests/tools.pp
+++ modules/buildmaster/manifests/tools.pp
@@ -7,7 +7,28 @@ class buildmaster::tools {
             python => "/tools/python27/bin/python2.7",
             require => Class['packages::mozilla::python27'],
             user => $users::builder::username,
-            group => $users::builder::group;
+            group => $users::builder::group,
+            packages => [
+                "Jinja2==2.5.5nja2==2.5.5",
+                "MySQL-python==1.2.3",
+                "SQLAlchemy==0.6.4",
+                "Twisted==10.1.0",
+                "amqplib==0.6.1",
+                "anyjson==0.3",
+                "argparse==1.1",
+                "carrot==0.10.7",
+                "distribute==0.6.14",
+                "pyOpenSSL==0.10",
+                "pyasn1==0.0.11a",
+                "pycrypto==2.3",
+                "pytz==2011d",
+                "wsgiref==0.1.2",
+                "zope.interface==3.6.1",
+                "mozillapulse==ad95569a089e",
+                "simplejson==2.1.6",
+                "buildbot==0.8.4_pre_moz1",
+            ],
+            cwd => buildmaster::settings::$master_root;
     }
     exec {
         "clone-tools":

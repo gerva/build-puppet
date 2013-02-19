cd56ffe51f6aff831d840230d07663c3a21e5e98
Massimo Gervasini <mgervasini@mozilla.com>
2013-02-19 09:02:48 +0100

    added facter my_num_masters - just a test

---
+++ modules/buildmaster/lib/facter/my_num_masters.rb
@@ -0,0 +1,11 @@
+# num_masters.rb
+#
+require 'facter'
+if FileTest.exists?("/etc/default/buildbot.d")
+        Facter.add("my_num_masters") do
+            setcode do
+                Facter::Util::Resolution.exec('ls -1 /etc/default/buildbot.d | wc -l')
+            end
+        end
+end
+

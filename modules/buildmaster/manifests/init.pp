a6be8188f73adcaa935ede26f229cf4574fed605
Massimo Gervasini <mgervasini@mozilla.com>
2013-02-26 11:51:26 +0100

    added nrpe::custom module

--- modules/buildmaster/manifests/init.pp
+++ modules/buildmaster/manifests/init.pp
@@ -12,6 +12,7 @@
 # TODO: you still have to set up ssh keys!
 class buildmaster {
     # TODO: port releng module
+    include nrpe::custom
     include buildmaster::queue
     include buildmaster::settings
     include tweaks::tcp_keepalive
@@ -27,10 +28,10 @@ class buildmaster {
             ensure => directory,
             owner => $users::builder::group,
             group => $users::builder::username;
-        "/etc/nagios/nrpe.d/buildbot.cfg":
-            content => template("buildmaster/buildbot.cfg.erb"),
-            notify => Class["nrpe::service"],
-            require => Package["nrpe"];
+#        "/etc/nagios/nrpe.d/buildbot.cfg":
+#            content => template("buildmaster/buildbot.cfg.erb"),
+#            notify => Class["nrpe::service"],
+#            require => Package["nrpe"];
         "/root/.my.cnf":
             content => template("buildmaster/my.cnf.erb"),
             mode => 600;
@@ -42,5 +43,9 @@ class buildmaster {
             mode => 755,
             recurse => true,
             force => true;
+    }
+    nrpe::custom {
+        "buildbot.cfg":
+            content => template("buildmaster/buildbot.cfg.erb");
     }
 }
---
+++ modules/nrpe/manifests/custom.pp
@@ -0,0 +1,13 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+class nrpe::custom($content) {
+    include nrpe::setting
+    $name=$title
+    file {
+        "${nrpe::settings::plugins_dir}/${name}":
+            content => $content
+            require => Package["nrpe"],
+            notify => Class['nrpe::service'];
+    }
+}

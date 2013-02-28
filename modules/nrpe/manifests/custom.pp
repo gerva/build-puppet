66b90ba8625f03599beedf8937c861b474688dfa
Massimo Gervasini <mgervasini@mozilla.com>
2013-02-28 14:01:42 +0100

    fixing nrpe::custom

--- modules/nrpe/manifests/custom.pp
+++ modules/nrpe/manifests/custom.pp
@@ -1,13 +1,15 @@
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
-class nrpe::custom($content) {
-    include nrpe::setting
-    $name=$title
+define nrpe::custom($nrpe_content) {
+    include nrpe::base
+    include nrpe::settings
+
     file {
-        "${nrpe::settings::plugins_dir}/${name}":
-            content => $content,
-            require => Package["nrpe"],
-            notify => Class['nrpe::service'];
+       "${nrpe::settings::plugins_dir}/${title}":
+            owner => root,
+            group => root,
+            notify => Class['nrpe::service'],
+            content => $nrpe_content;
     }
 }

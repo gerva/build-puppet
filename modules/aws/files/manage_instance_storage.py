2e21f01269d1b2bc42a8a8129d86b68cadc896f2
Massimo Gervasini <mgervasini@mozilla.com>
2014-04-03 17:49:19 +0200

    updated debug strings

--- modules/aws/files/manage_instance_storage.py
+++ modules/aws/files/manage_instance_storage.py
@@ -67,12 +67,12 @@ def get_output_from_cmd(cmd, cwd=None, raise_on_error=True):
     log.debug("Running %s cwd: %s", cmd, cwd)
     # check_output is not avalilable in prod (python 2.6)
     # return check_output(cmd, cwd=cwd, stderr=None).splitlines()
-    log.debug("cmd: {0}; cwd={1}".format(cmd, cwd))
+    log.debug("cmd: %s; cwd=%s", cmd, cwd)
     proc = Popen(cmd, cwd=cwd, stdout=PIPE)
     output, err = proc.communicate()
     retcode = proc.poll()
     if retcode and raise_on_error:
-        log.debug('cmd: {0} returned {1} ({2})'.format(cmd, retcode, err))
+        log.debug('cmd: %s returned %s (%s)', cmd, retcode, err)
         raise CalledProcessError(retcode, cmd, output)
     return output
 
@@ -106,7 +106,7 @@ def aws2xen(device):
 def format_device(device):
     """formats the disk with ext4 fs if needed"""
     if is_mounted(device):
-        log.debug('{0} is mounted: skipping formatting')
+        log.debug('%s is mounted: skipping formatting', device)
         return
     # assuming this device needs to be formatted
     need_format = True
@@ -118,17 +118,17 @@ def format_device(device):
                'ID_FS_TYPE=ext3' in line:
                # if the disk is already ext4 or ext3, do not format
                 need_format = False
-                log.info('{0} no need to format: {1}'.format(device, line))
+                log.info('%s no need to format: %s', device, line)
                 break
     if need_format:
-        log.info('formatting {0}'.format(device))
+        log.info('formatting %s', device)
         run_cmd(['mkfs.ext4', device])
 
 
 def needs_pvcreate(device):
     """checks if pvcreate is needed"""
     output = get_output_from_cmd('pvs')
-    log.debug("pvs output for device {0}: {1} ".format(device, output))
+    log.debug("pvs output for device %s: %s ", device, output)
     for line in output.splitlines():
         if device in line:
             return False
@@ -139,18 +139,17 @@ def lvmjoin(devices):
     "Creates a single lvm volume from a list of block devices"
     for device in devices:
         if needs_pvcreate(device):
-            log.info('clearing the partition table for {0}'.format(device))
+            log.info('clearing the partition table for %s', device)
             run_cmd(['dd', 'if=/dev/zero', 'of=%s' % device,
                      'bs=512', 'count=1'])
-            log.info('creating a new physical volume for: {0}'.format(device))
+            log.info('creating a new physical volume for: %s', device)
             run_cmd(['pvcreate', '-ff', '-y', device])
     vg_name = 'vg'
     lv_name = 'local'
     if not run_cmd(['vgdisplay', vg_name], raise_on_error=False):
-        log.info('creating a new volume group, {0} with {1}'.format(vg_name,
-                                                                    devices))
+        log.info('creating a new volume group, %s with %s', vg_name, devices)
         run_cmd(['vgcreate', vg_name] + devices)
-    lv_path = "/dev/mapper/%s-%s" % (vg_name, lv_name)
+    lv_path = "/dev/mapper/%s-%s", vg_name, lv_name
     if not run_cmd(['lvdisplay', lv_path], raise_on_error=False):
         log.info('creating a new logical volume')
         run_cmd(['lvcreate', '-l', '100%VG', '--name', lv_name, vg_name])
@@ -164,7 +163,7 @@ def fstab_line(device):
     for line in read_fstab():
         if not line.startswith('#') \
            and device in line:
-            log.debug("{0} already in /etc/fstab:".format(device))
+            log.debug("%s already in /etc/fstab:", device)
             log.debug(line)
             is_fstab_line = line
             break
@@ -181,18 +180,17 @@ def update_fstab(device, mount_location):
     """Updates /etc/fstab if needed"""
     # example:
     # /dev/sda / ext4 defaults,noatime  1 1
-    new_fstab_line = '{0} {1} ext4 defaults,noatime 0 0\n'.format(
-                     device, mount_location)
+    new_fstab_line = '%s %s ext4 defaults,noatime 0 0\n', device, mount_location
     old_fstab_line = fstab_line(device)
     if old_fstab_line == new_fstab_line:
         # nothing to do..
-        log.debug('{0} already in /etc/fstab'.format(new_fstab_line))
+        log.debug('%s already in /etc/fstab', new_fstab_line)
         return
     # needs to be added
     if not old_fstab_line:
         with open('/etc/fstab', 'a') as out_f:
             out_f.write(new_fstab_line)
-        log.debug('added {0} in /etc/fstab'.format(new_fstab_line))
+        log.debug('added %s in /etc/fstab', new_fstab_line)
         return
     # just in case...
     # log fstab content before updating it
@@ -204,8 +202,8 @@ def update_fstab(device, mount_location):
     with open(temp_fstab.name, 'w') as out_fstab:
         for line in old_fstab:
             out_fstab.write(line.replace(old_fstab_line, new_fstab_line))
-        log.debug('replaced {0} with: {1} in /etc/fstab'.format(old_fstab_line,
-                                                                new_fstab_line))
+        log.debug('replaced %s with: %s in /etc/fstab', old_fstab_line,
+                  new_fstab_line)
     os.rename(temp_fstab.name, '/etc/fstab')
 
 
@@ -216,8 +214,8 @@ def get_builders_from(jacuzzi_metadata_file):
         with open(jacuzzi_metadata_file) as data_file:
             json_data = json.load(data_file)
     except (IOError, ValueError, AttributeError):
-        log.debug('{0} does not exist or it cannot be decoded or is None'
-                  .format(jacuzzi_metadata_file))
+        log.debug('%s does not exist or it cannot be decoded or is None',
+                  jacuzzi_metadata_file)
         return []
     try:
         return json_data['builders']
@@ -239,13 +237,13 @@ def mount_point():
 def is_mounted(device):
     """checks if a device is mounted"""
     mount_out = get_output_from_cmd('mount')
-    log.debug("mount: {0}".format(mount_out))
+    log.debug("mount: %s", mount_out)
     for line in mount_out.splitlines():
         log.debug(line)
         if device in line:
-            log.debug('device: {0} is mounted'.format(device))
+            log.debug('device: %s is mounted', device)
             return True
-    log.debug('device: {0} is not mounted'.format(device))
+    log.debug('device: %s is not mounted', device)
     return False
 
 
@@ -253,9 +251,9 @@ def mount(device):
     """mounts device according to fstab"""
     mount_p = mount_point()
     if not os.path.exists(mount_p):
-        log.debug('Creating directory {0}'.format(mount_p))
+        log.debug('Creating directory %s', mount_p)
         os.makedirs(mount_p)
-    log.info('mounting {0}'.format(device))
+    log.info('mounting %s', device)
     run_cmd(['mount', device])
 
 
@@ -269,14 +267,14 @@ def main():
         return
     if len(devices) > 1:
         # requires lvm
-        log.info('found devices: {0}'.format(devices))
+        log.info('found devices: %s', devices)
         device = lvmjoin(devices)
     else:
         # single device no need for lvm, just format
         device = devices[0]
-        log.info('found device: {0}'.format(device))
+        log.info('found device: %s', device)
         format_device(device)
-    log.info("Got {0}".format(device))
+    log.info("Got %s", device)
     update_fstab(device, mount_point())
     if not is_mounted(device):
         mount(device)
